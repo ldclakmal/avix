7dc0bc35487caaba47fcdd426c1156673c6f6696
Chanaka Lakmal <ldclakmal@gmail.com>
2017-03-27 10:43:33 +0530

    Initial commit with old template

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="description" content="The Official Web Site of AVIX | Photography">
+        <meta name="keywords" content="avix, avix.lk" />
+        <title>AVIX&reg; | Official</title>
+        <link rel="shortcut icon" href="images/favicon.png">
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap-light.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme-light.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/style-light.css" rel="stylesheet">
+
+        <!-- Superslides CSS -->
+        <link href="css/superslides.css" rel="stylesheet">
+
+        <!-- Owl Carousel stylesheets -->
+        <link href="css/owl.carousel.css" rel="stylesheet">
+        <link href="css/owl.theme.css" rel="stylesheet">
+
+        <!-- Fancybox -->
+        <link href="css/jquery.fancybox.css" rel="stylesheet">
+
+        <!-- Animate - necessary for Wow.js -->
+        <link href="css/animate.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/custom.css" rel="stylesheet">
+
+        <!-- Google Fonts -->
+        <!--<link href="http://fonts.googleapis.com/css?family=PT+Sans:400,700|Oranienbaum|PT+Serif:400italic" rel="stylesheet">-->
+
+        <!-- Font Awesome Icons -->
+        <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.2/css/font-awesome.css" rel="stylesheet">
+
+    </head>
+    <body data-spy="scroll" data-target=".navbar-collapse">
+
+        <div id="pre-loader" class="preloader-div">
+            <h4 class="preloader-h4">
+                <span><img src="images/logo-blue.png" height="250" id="logo"></span>
+            </h4>
+        </div>
+
+        <div id="background">            
+            <div id="background-center"></div>
+            <div id="background-left" class="background-component"></div>
+            <div id="background-right" class="background-component"></div>
+            <div id="background-overlay"></div>
+        </div>
+
+        <!-- ========== MENU START ========== -->
+
+        <div class="navbar navbar-fixed-top" style="background-color: #1f81fa;">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="index.php">
+                        <img src="images/logo.png" style="height: 60px; padding-top: 10px;" alt="AVIX -We Delight You-" class="img-responsive" />
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- ========== MENU END ========== -->
+
+        <!-- ========== BANNER START ========== -->
+
+        <div id="banner">
+            <div class="slider">
+                <div class="slides-container">
+                    <img src="images/slide/slide1.jpg" alt="" />
+                    <img src="images/slide/slide2.jpg" alt="" />
+                    <img src="images/slide/slide3.jpg" alt="" />
+                    <img src="images/slide/slide4.jpg" alt="" />
+                </div>
+            </div>
+            <div class="tint">
+                <div class="welcome text-center">
+                    <h1><span>WELCOME TO</span><span style="color: #fdb515; font-size: 80px; font-weight: bold; font-family: 'Open Sans'">AVIX Productions</span></h1>
+                    <h3>- We Delight You -</h3>
+                    <p><a href="https://www.facebook.com/avixproductions/" class="btn btn-primary btn-lg">We will be back soon!</a></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- ========== BANNER END ========== -->
+
+        <div id="home"></div>        
+
+        <!-- ========== FOOTER START ========== -->
+
+        <footer>
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-sm-12">
+                        <p>
+                            <span><a href="https://www.facebook.com/avixproductions/"><i class="fa fa-facebook fa-lg"></i></a></span>
+                            <span><a href="https://www.linkedin.com/company-beta/6592781/"><i class="fa fa-linkedin fa-lg"></i></a></span>
+                            <span><i class="fa fa-phone fa-lg"></i> +94 76 763 2424 &nbsp; <i class="fa fa-phone fa-lg"></i> +94 71 763 2424</span>
+                            <span><i class="fa fa-envelope fa-lg"></i><a href="mailto:info@avix.lk"> info@avix.lk</a></span>
+                            <span><i class="fa fa-map-marker fa-lg"></i> Battaramulla, Colombo, Sri Lanka. 10120</span>
+                        </p>
+                        <!--<p>&copy; 2014 Scent Model Agency</p>-->
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- ========== FOOTER END ========== -->
+
+        <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"></div>
+
+        <!-- Modernizr Plugin -->
+        <script src="js/modernizr.custom.97074.js"></script>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/jquery-1.10.2.min.js"></script>
+
+        <!-- Bootstrap Plugins -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Retina Plugin -->
+        <script src="js/retina-1.1.0.min.js"></script>
+
+        <!-- Superslides Plugin -->
+        <script src="js/jquery.easing.1.3.js"></script>
+        <script src="js/jquery.animate-enhanced.min.js"></script>
+        <script src="js/jquery.superslides.js"></script>
+
+        <!-- Owl Carousel Plugin -->
+        <script src="js/owl.carousel.js"></script>
+
+        <!-- Scrolly Plugin -->
+        <script src="js/jquery.scrolly.js"></script>
+
+        <!-- MixItUp Plugin -->
+        <script src="js/jquery.mixitup.min.js"></script>
+
+        <!-- Fancybox Plugin -->
+        <script src="js/wow.min.js"></script>
+        <script>
+            new WOW().init();
+        </script>
+
+        <!-- WOW Plugin -->
+        <script src="js/jquery.fancybox.js"></script>
+
+        <!-- Contact form processing plugin -->
+        <script src="js/magic.js"></script>
+
+        <!-- jQuery Settings -->
+        <script src="js/settings.js"></script>
+
+        <!-- Bootstrap Modal -->
+        <script src="js/modal.js"></script>
+        <script src="js/popup.js"></script>
+
+        <!-- Navigations -->
+        <script src="js/navigations.js"></script>
+    </body>
+</html>
