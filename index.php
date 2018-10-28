c3919b444a16a0998753d70812b5ebcbecd15a36
Chanaka Lakmal <ldclakmal@gmail.com>
2018-10-28 16:01:36 +0530

    Update home page

--- index.php
+++ index.php
@@ -32,13 +32,13 @@
         <link href="css/custom.css" rel="stylesheet">
 
         <!-- Google Fonts -->
-        <!--<link href="http://fonts.googleapis.com/css?family=PT+Sans:400,700|Oranienbaum|PT+Serif:400italic" rel="stylesheet">-->
+        <link href="http://fonts.googleapis.com/css?family=PT+Sans:400,700|Oranienbaum|PT+Serif:400italic" rel="stylesheet">
 
         <!-- Font Awesome Icons -->
         <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.2/css/font-awesome.css" rel="stylesheet">
 
     </head>
-    <body data-spy="scroll" data-target=".navbar-collapse">
+    <body data-spy="scroll" data-target=".navbar-collapse" style="overflow-y: hidden;">
 
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -51,77 +51,40 @@
 
     </script>
 
-        <div id="pre-loader" class="preloader-div">
-            <h4 class="preloader-h4">
-                <span><img src="images/logo-blue.png" height="250" id="logo"></span>
-            </h4>
-        </div>
-
-        <div id="background">            
-            <div id="background-center"></div>
-            <div id="background-left" class="background-component"></div>
-            <div id="background-right" class="background-component"></div>
-            <div id="background-overlay"></div>
-        </div>
-
-        <!-- ========== MENU START ========== -->
-
-        <div class="navbar navbar-fixed-top" style="background-color: #1f81fa;">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a href="index.php">
-                        <img src="images/logo.png" style="height: 60px; padding-top: 10px;" alt="AVIX - We Delight You-" class="img-responsive" />
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <!-- ========== MENU END ========== -->
-
         <!-- ========== BANNER START ========== -->
 
         <div id="banner">
             <div class="slider">
                 <div class="slides-container">
-                    <img src="images/slide/slide1.jpg" alt="" />
-                    <img src="images/slide/slide2.jpg" alt="" />
-                    <img src="images/slide/slide3.jpg" alt="" />
-                    <img src="images/slide/slide4.jpg" alt="" />
+                    <img src="images/slide/bg-slide.jpg" alt="" />
+                    <img src="images/slide/bg-slide.jpg" alt="" />
                 </div>
             </div>
             <div class="tint">
-                <div class="welcome text-center">
-                    <h1><span>WELCOME TO</span></h1>
-                    <img src="images/logo-blue.png" style="height: 150px;"/>
-                    <p><a href="https://www.facebook.com/avixproductions/" class="btn btn-primary btn-lg">Stay Tuned!</a></p>
+                <div class="welcome">
+                    <img src="images/logo-teal.png" style="height: 150px;"/>
+                    <p style="padding-left: 5%;"><a href="https://www.facebook.com/avixproductions/" class="btn btn-default">STAY TUNED!</a></p>
                 </div>
             </div>
         </div>
 
         <!-- ========== BANNER END ========== -->
 
-        <div id="home"></div>        
+        <div id="home"></div>
 
         <!-- ========== FOOTER START ========== -->
 
-        <footer>
+        <footer class="navbar-fixed-bottom">
             <div class="container">
                 <div class="row text-center">
                     <div class="col-sm-12">
                         <p>
-                            <span><a href="https://www.facebook.com/avixproductions/"><i class="fa fa-facebook fa-lg"></i> AVIX Productions</a></span>
-                            <span><a href="https://www.linkedin.com/company/avixproductions"><i class="fa fa-linkedin fa-lg"></i>  AVIX Productions</a></span>
+                            <span><a href="https://www.facebook.com/avixproductions/" target="_blank"><i class="fa fa-facebook fa-lg"></i> AVIX Productions</a></span>
+                            <span><a href="https://www.instagram.com/avixproductions/" target="_blank"><i class="fa fa-instagram fa-lg"></i>  AVIX Productions</a></span>
                             <span><i class="fa fa-phone fa-lg"></i> +94 76 763 2424</span>
-                            <span><i class="fa fa-phone fa-lg"></i> +94 71 763 2424</span>
                             <span><i class="fa fa-envelope fa-lg"></i><a href="mailto:info@avix.lk"> info@avix.lk</a></span>
                             <span><i class="fa fa-map-marker fa-lg"></i> Battaramulla, Colombo, Sri Lanka. 10120</span>
                         </p>
-                        <!--<p>&copy; 2014 Scent Model Agency</p>-->
                     </div>
                 </div>
             </div>
