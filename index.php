97c4ac0eb9cfd1bab806a052269157638711178b
Chanaka Lakmal <ldclakmal@gmail.com>
2019-03-24 11:51:26 +0530

    Remove unused imports

--- index.php
+++ index.php
@@ -3,30 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <meta name="description" content="The Official Web Site of AVIX | Photography">
+        <meta name="description" content="The Official Web Site of AVIX.LK | Weddings | Photography | Videography">
         <meta name="keywords" content="avix, avix.lk" />
         <title>AVIX&reg; | Official</title>
         <link rel="shortcut icon" href="images/favicon.png?v=2">
-
-        <!-- Bootstrap -->
-        <link href="css/bootstrap-light.min.css" rel="stylesheet">
-        <link href="css/bootstrap-theme-light.min.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
-        <link href="css/style-light.css" rel="stylesheet">
-
-        <!-- Superslides CSS -->
-        <link href="css/superslides.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
-        <link href="css/custom.css" rel="stylesheet">
-
-        <!-- Google Fonts -->
-        <link href="http://fonts.googleapis.com/css?family=PT+Sans:400,700|Oranienbaum|PT+Serif:400italic" rel="stylesheet">
-
-        <!-- Font Awesome Icons -->
-        <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.2/css/font-awesome.css" rel="stylesheet">
-
+        <link rel="stylesheet" href="css/bootstrap-light.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style-light.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" crossorigin="anonymous">
     </head>
     <body data-spy="scroll" data-target=".navbar-collapse" style="overflow-y: hidden;">
 
@@ -38,11 +21,7 @@
 
         ga('create', 'UA-69533863-2', 'auto');
         ga('send', 'pageview');
-
     </script>
-
-        <!-- ========== BANNER START ========== -->
-
         <div id="banner">
             <div class="slider">
                 <div class="slides-container">
@@ -59,27 +38,21 @@
                         <img src="images/logo.png" class="col-lg-3 col-md-6 col-sm-6 col-xs-12"/>
                     </div>
                     <div class="col-lg-11 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12" style="left: 5%;">
-                        <a href="https://www.facebook.com/avixweddings/" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-lg"></i></a>
-                        <a href="https://www.instagram.com/avixproductions/" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-instagram fa-lg"></i></a>
+                        <a href="https://www.facebook.com/avixweddings/" target="_blank" class="btn btn-default btn-lg"><i class="fab fa-facebook fa-lg"></i></a>
+                        <a href="https://www.instagram.com/avixproductions/" target="_blank" class="btn btn-default btn-lg"><i class="fab fa-instagram fa-lg"></i></a>
                         <a href="mailto:info@avix.lk" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-envelope fa-lg"></i></a>
                     </div>
                 </div>
             </div>
         </div>
 
-        <!-- ========== BANNER END ========== -->
-
-        <div id="home"></div>
-
-        <!-- ========== FOOTER START ========== -->
-
         <footer class="navbar-fixed-bottom">
             <div class="container">
                 <div class="row text-center">
                     <div class="col-sm-12">
                         <p>
-                            <span><a href="https://www.facebook.com/avixweddings/" target="_blank"><i class="fa fa-facebook fa-lg"></i> AVIX Productions</a></span>
-                            <span><a href="https://www.instagram.com/avixproductions/" target="_blank"><i class="fa fa-instagram fa-lg"></i>  AVIX Productions</a></span>
+                            <span><a href="https://www.facebook.com/avixweddings/" target="_blank"><i class="fab fa-facebook fa-lg"></i> AVIX Productions</a></span>
+                            <span><a href="https://www.instagram.com/avixproductions/" target="_blank"><i class="fab fa-instagram fa-lg"></i>  AVIX Productions</a></span>
                             <span><i class="fa fa-phone fa-lg"></i><a href="tel:+94767632424"> +94 76 763 2424</a></span>
                             <span><i class="fa fa-envelope fa-lg"></i><a href="mailto:info@avix.lk"> info@avix.lk</a></span>
                             <span><i class="fa fa-map-marker fa-lg"></i><a href="https://goo.gl/maps/ukPvzpBUR2N2" target="_blank"> Battaramulla, Colombo, Sri Lanka. 10120</a></span>
@@ -89,25 +62,8 @@
             </div>
         </footer>
 
-        <!-- ========== FOOTER END ========== -->
-
-        <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"></div>
-
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="js/jquery-1.10.2.min.js"></script>
-
-        <!-- Bootstrap Plugins -->
-        <script src="js/bootstrap.min.js"></script>
-
-        <!-- Retina Plugin -->
-        <script src="js/retina-1.1.0.min.js"></script>
-
-        <!-- Superslides Plugin -->
-        <script src="js/jquery.easing.1.3.js"></script>
-        <script src="js/jquery.animate-enhanced.min.js"></script>
         <script src="js/jquery.superslides.js"></script>
-
-        <!-- jQuery Settings -->
         <script src="js/settings.js"></script>
     </body>
 </html>
