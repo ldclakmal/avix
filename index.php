3102f11829190869a2dbe4d4e36f85be2e5fe171
Chanaka Lakmal <ldclakmal@gmail.com>
2019-03-24 12:06:26 +0530

    Update google analytics

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-69533863-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-69533863-2');
+        </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <meta name="description" content="The Official Web Site of AVIX.LK | Weddings | Photography | Videography">
@@ -12,16 +21,6 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" crossorigin="anonymous">
     </head>
     <body data-spy="scroll" data-target=".navbar-collapse" style="overflow-y: hidden;">
-
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-69533863-2', 'auto');
-        ga('send', 'pageview');
-    </script>
         <div id="banner">
             <div class="slider">
                 <div class="slides-container">
